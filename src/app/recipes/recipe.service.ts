011fad3e8ce771dd6c52077653e15fe40ecc3335
Daniel Becerra <danieljosebecerra@gmail.com>
2016-12-25 03:10:46 -0430

    cross service communication, updating shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Recipe } from './recipe';
+import { Ingredient } from '../shared/ingredient';
 
 @Injectable()
 export class RecipeService {
   private recipes: Recipe[] = [
-    new Recipe('Dummy', 'Dummy', 'http://corporacion3018.net.ve/res/img/site/Dummy1.jpg', []),
+    new Recipe('Dummy', 'Dummy', 'http://corporacion3018.net.ve/res/img/site/Dummy1.jpg', [
+      new Ingredient('french fries', 2),
+      new Ingredient('pork meat', 1)
+    ]),
     new Recipe('Dummy1', 'Dummy1', 'http://corporacion3018.net.ve/res/img/site/Dummy1.jpg', []),
     new Recipe('Dummy2', 'Dummy2', 'http://corporacion3018.net.ve/res/img/site/Dummy1.jpg', [])
   ];
