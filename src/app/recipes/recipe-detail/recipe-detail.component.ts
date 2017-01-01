1cf68474b359f29ea9d1e446fa402a9f271295c0
Daniel Becerra <danieljosebecerra@gmail.com>
2017-01-01 02:20:00 -0430

    adding child route navigation

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { Recipe } from '../recipe';
 import { ShoppingListService } from '../../shopping-list/shopping-list.service';
+
 @Component({
   selector: 'rb-recipe-detail',
   templateUrl: './recipe-detail.component.html'
 })
 export class RecipeDetailComponent implements OnInit {
   @Input() selectedRecipe: Recipe;
-  constructor(private sls: ShoppingListService) { }
+  private recipeIndex: number = 1;
+
+  constructor(private sls: ShoppingListService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onEdit() {
+    this.router.navigate(['/recipes', this.recipeIndex, 'edit']);
+  }
+
+  onDelete() {
+    this.router.navigate(['/recipes']);
   }
 
   onAddToShoppingList() {
